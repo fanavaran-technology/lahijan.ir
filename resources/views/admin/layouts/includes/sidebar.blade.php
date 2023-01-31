fb6583980fc4e771872c8e06134cf5a818f02ee0
yasin taghavi <mrtqvi@yahoo.com>
2023-01-31 07:43:40 +0330

    add menu item to sidebar

--- resources/views/admin/layouts/includes/sidebar.blade.php
+++ resources/views/admin/layouts/includes/sidebar.blade.php
@@ -76,6 +76,23 @@
           </ul>
         </li>
         <li class="nav-item dropdown">
+          <a href="#menu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-menu-up" viewBox="0 0 16 16">
+              <path d="M7.646 15.854a.5.5 0 0 0 .708 0L10.207 14H14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h3.793l1.853 1.854zM1 9V6h14v3H1zm14 1v2a1 1 0 0 1-1 1h-3.793a1 1 0 0 0-.707.293l-1.5 1.5-1.5-1.5A1 1 0 0 0 5.793 13H2a1 1 0 0 1-1-1v-2h14zm0-5H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v2zM2 11.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 0-1h-8a.5.5 0 0 0-.5.5zm0-4a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11a.5.5 0 0 0-.5.5zm0-4a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1h-6a.5.5 0 0 0-.5.5z"/>
+            </svg>
+            <span class="ml-3 item-text">منو ها</span>
+          </a>
+          <ul class="collapse list-unstyled pl-4 w-100" id="menu">
+            <li class="nav-item">
+              <a class="nav-link pl-3" href="{{ route('admin.content.menus.index') }}"><span class="ml-1 item-text">همه منو ها </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link pl-3" href="{{ route('admin.content.menus.create') }}"><span class="ml-1 item-text">منوی جدید</span></a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
           <a href="#shafaf" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
             <i class="fe fe-pie-chart fe-16"></i>
             <span class="ml-3 item-text">شفاف سازی</span>
