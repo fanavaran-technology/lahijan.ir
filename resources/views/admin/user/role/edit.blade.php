0e106459d5d35d41b0dfdc96615faef24eee097b
yasin taghavi <mrtqvi@yahoo.com>
2023-02-14 16:35:16 +0330

    changes in the roles section

--- resources/views/admin/user/role/edit.blade.php
+++ resources/views/admin/user/role/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.layouts.app', ['title' => ' اسلایدر جدید'])
 
 @section('head-tag')
-
 @endsection
 
 @section('content')
@@ -36,15 +35,42 @@
                                         <div class="col-md-6 mb-3">
                                             <label for="validationCustom3">عنوان نقش</label>
                                             <input type="text" name="title" placeholder="عنوان نقش را وراد کنید""
-                                                class="form-control" id="validationCustom3" value="{{ old('title' , $role->title) }}">
+                                                class="form-control" id="validationCustom3"
+                                                value="{{ old('title', $role->title) }}">
                                         </div>
                                         <div class="col-md-6 mb-3">
                                             <label for="validationCustom3">توضیح نقش</label>
                                             <input type="text" name="description" placeholder="توضیح نقش را وراد کنید""
-                                                class="form-control" id="validationCustom3" value="{{ old('description' , $role->description) }}">
+                                                class="form-control" id="validationCustom3"
+                                                value="{{ old('description', $role->description) }}">
                                         </div>
                                     </div>
-                                    <button class="btn btn-primary" type="submit">ثبت کنید</button>
+                                    <section class="col-12">
+                                        <div class="custom-control custom-checkbox pl-2">
+                                            <input type="checkbox" class="custom-control-input" id="select-all">
+                                            <label class="custom-control-label" for="select-all">انتخاب همه دسترسی ها</label>
+                                        </div>
+                                        <section class="row border-top mt-3 py-3">
+                                            @php
+                                                $rolePermissions = $role->permissions->pluck('id')->toArray();   
+                                            @endphp
+                                            @foreach ($permissions as $key => $permission)
+                                                <section class="col-md-3 mt-2">
+                                                    <div class="custom-control custom-checkbox pl-2">
+                                                        <input type="checkbox"  @checked(in_array($permission->id , old('permissions' , $rolePermissions) ?? [])) class="custom-control-input permission-check"
+                                                            name="permissions[]" value="{{ $permission->id }}"
+                                                            id="{{ $permission->id }}">
+                                                        <label class="custom-control-label"
+                                                            for="{{ $permission->id }}">{{ $permission->title }}</label>
+                                                        @error('permissions.'. $key)
+                                                            <span class="block text-xs text-red-500 dark:text-red-400">{{ $message }}</span>
+                                                        @enderror
+                                                    </div>
+                                                </section>
+                                            @endforeach
+                                        </section>
+                                    </section>
+                                    <button class="btn btn-primary mt-2" type="submit">ثبت کنید</button>
                                 </form>
                             </div> <!-- /.card-body -->
                         </div> <!-- /.card -->
@@ -57,4 +83,21 @@
 @endsection
 
 @section('script')
+<script>
+    const selectAll = document.querySelector('#select-all');
+    selectAll.addEventListener('change' , e => {
+        console.log('hi');
+        const permissions = document.querySelectorAll('.permission-check');
+
+        if (e.target.checked) {
+            permissions.forEach(permission => {
+                permission.checked = true;
+            });
+        } else {
+            permissions.forEach(permission => {
+                permission.checked = false;
+            });
+        }
+    });
+</script>
 @endsection
--- resources/views/admin/user/role/index.blade.php
+++ resources/views/admin/user/role/index.blade.php
@@ -5,11 +5,11 @@
         <div class="col">
             <h2 class="h3 mb-0 page-title">همه نقش ها</h2>
         </div>
-      @can('create_roles')
-      <div class="col-auto">
-        <a href="{{ route('admin.user.roles.create') }}" type="button" class="btn btn-primary px-4">ایجاد</a>
-    </div>
-      @endcan
+        @can('create_roles')
+            <div class="col-auto">
+                <a href="{{ route('admin.user.roles.create') }}" type="button" class="btn btn-primary px-4">ایجاد</a>
+            </div>
+        @endcan
         <div class="col-12">
 
             <div class="row my-4">
@@ -21,72 +21,105 @@
                             <table class="table table-striped" id="table-id">
                                 <thead>
                                     <div class="form-row py-2">
-                                        <input class="col-md-3 form-control form-group" type="text"
-                                            placeholder=" نفقش خود را جستجو جو کنید ...">
+                                        <form action="">
+                                            <input name="search" class="col-md-3 form-control custom-focus form-group"
+                                                type="text" placeholder="عنوان را جستجو و enter کنید">
+                                        </form>
+                                    </div>
+                                    <div class="row w-100 mb-4 ml-1">
+                                        @request('search')
+                                            <h5>
+                                                <span class="badge bg-light text-dark border mr-2">
+                                                    جستجو : {{ request('search') }}
+                                                    <svg style="cursor:pointer" class="ml-4" onclick="removeFilter('search')"
+                                                        xmlns="http://www.w3.org/2000/svg" width="12" height="12"
+                                                        fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                                                    </svg>
+                                                </span>
+                                            </h5>
+                                        @endrequest
                                     </div>
                                     <th>#</th>
                                     <th>نام نقش</th>
                                     <th>دسترسی ها</th>
-                                    <th>ویرایش دسترسی</th>
                                     <th>عملیات</th>
                                     </tr>
                                 </thead>
                                 @foreach ($roles as $role)
-                                
-                                <tr class="flex item-center">
-                                        
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $role->title }}</td>
-                                    <td>
-                                        @if (empty($role->permissions()->get()->toArray()))
-                                            <span class="text-danger">برای این نقش هیچ دسترسی وجود ندارد</span>
-                                            @else
-                                            @foreach ($role->permissions as $permission )
-                                                {{ $permission->title }}<br>
-                                            @endforeach
-                                        @endif
-                                    </td>             
-                                    <td>
-                                       @can('set_user_permission_role')
-                                       <a href="{{ route('admin.user.roles.permission-form' , $role->id) }}" type="button" class="btn btn-success ">
-                                        دسترسی ها
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-universal-access" viewBox="0 0 16 16">
-                                            <path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM6 5.5l-4.535-.442A.531.531 0 0 1 1.531 4H14.47a.531.531 0 0 1 .066 1.058L10 5.5V9l.452 6.42a.535.535 0 0 1-1.053.174L8.243 9.97c-.064-.252-.422-.252-.486 0l-1.156 5.624a.535.535 0 0 1-1.053-.174L6 9V5.5Z"/>
-                                          </svg>
-                                        </a>
-                                       @endcan
-                                    </a>
-                                </td>
-                                    <td>
-                                        @can('edit_roles')
-                                        <a href="#" class="text-decoration-none text-info mr-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                                            </svg>
-                                        </a>
-                                        @endcan
-                                        @can('delete_roles')
-                                        <form action="{{ route('admin.user.roles.destroy' , $role->id) }}" class="d-inline" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" x-data="{{ $role->id }}" class="delete border-none bg-transparent text-decoration-none text-danger mr-3">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                                </svg>
-                                            </a>
-                                        </form>
-                                        @endcan
-                                    </td>
-                                </tr>
-                                @endforeach
+                                    <tr class="flex item-center">
 
-                              
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $role->title }}</td>
+                                        <td>
+                                            @forelse ($role->permissions->take(5) as $permission)
+                                                {{ $permission->title }}<br>
+                                            @empty
+                                                <span class="text-danger">برای این نقش هیچ دسترسی وجود ندارد</span>
+                                            @endforelse
+                                            @if (count($role->permissions) > 5)
+                                            <a href="#{{$role->title}}" onclick="showPermissions({{ $role->id }})">نمایش همه</a>
+                                            <div class="modal role-permissions-{{ $role->id }}" tabindex="-1">
+                                                <div class="modal-dialog">
+                                                  <div class="modal-content">
+                                                    <div class="modal-header">
+                                                      <h5 class="modal-title">مجوز های نقش {{ $role->title }}</h5>
+                                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                      </button>
+                                                    </div>
+                                                    <div class="modal-body" style="max-height: 65vh; overflow: auto">
+                                                        @foreach ($role->permissions as $permission)
+                                                        <p>{{ $permission->title }}<p>
+                                                        @endforeach
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
+                                                        <a href="{{ route('admin.user.roles.edit' , $role->id) }}" class="btn btn-primary">ویرایش</a>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                            </div>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @can('edit_roles')
+                                                <a href="{{ route('admin.user.roles.edit', $role->id) }}"
+                                                    class="text-decoration-none text-primary mr-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                        fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                        <path fill-rule="evenodd"
+                                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                    </svg>
+                                                </a>
+                                            @endcan
+                                            @can('delete_roles')
+                                                <form action="{{ route('admin.user.roles.destroy', $role->id) }}"
+                                                    class="d-inline" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" x-data="{{ $role->id }}"
+                                                        class="delete border-none bg-transparent text-decoration-none text-danger mr-3">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                            fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                            <path fill-rule="evenodd"
+                                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                        </svg>
+                                                        </a>
+                                                </form>
+                                            @endcan
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </table>
-                            {{-- <section class="d-flex justify-content-center">
-                                {{ $pages->render() }}
-                            </section> --}}
+                            <section class="d-flex justify-content-center">
+                                {{ $roles->render() }}
+                            </section>
                         </div>
                     </div> <!-- simple table -->
                 </div> <!-- end section -->
@@ -97,8 +130,13 @@
 
 
 @section('script')
+    @include('admin.alerts.confirm')
 
-
-@include('admin.alerts.confirm')
-
+    <script>
+        function showPermissions(roleId) {
+            $('.role-permissions-'+roleId).modal({
+                keyboard: false
+            })
+        }
+    </script>
 @endsection
