bffc1970f29778326102f6ca85bbb2c976207d0f
yasin taghavi <mrtqvi@yahoo.com>
2023-09-11 14:03:02 +0330

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('backup:run');
+        $schedule->command('php artisan backup:run');
     }
 
     /**
