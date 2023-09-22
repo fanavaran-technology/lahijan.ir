6164e61c05dd1bea8a451730a4464da63bfde35b
yasin taghavi <mrtqvi@yahoo.com>
2023-09-22 12:33:49 +0330

    add complaint module and build its tables

--- Modules/Complaint/Database/Migrations/2023_09_22_081159_create_complaints_table.php
+++ Modules/Complaint/Database/Migrations/2023_09_22_081159_create_complaints_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class CreateComplaintsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -26,12 +26,19 @@ return new class extends Migration
             $table->string('builing_name')->nullable()->comment('نام مجتمع');
             $table->string('tracking_code');
             $table->longText("description");
-            $table->foreignId('department_id')->nullable()->constrained('departments');
             $table->foreignId('reference_id')->nullable()->constrained('users');
-            $table->tinyInteger('is_failed')->default(0)->comment('1 is failed');
+            $table->tinyInteger('is_answered')->default(0)->comment('1 is answered');
             $table->longText('answer')->nullable();
             $table->timestamp('referenced_at')->nullable();
             $table->timestamp('answered_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('complaint_user_fails', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('complaint_id')->constrained('complaints');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('departement_id')->constrained('departements');
             $table->timestamps();
         });
     }
@@ -43,6 +50,7 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('complaint_user_fails');
         Schema::dropIfExists('complaints');
     }
-};
+}
