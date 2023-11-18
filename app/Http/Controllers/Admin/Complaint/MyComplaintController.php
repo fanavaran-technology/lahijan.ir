708f1fd89aff0ff98eaa423121ce7f4e892e6bfd
yasin taghavi <mrtqvi@yahoo.com>
2023-11-18 15:25:59 +0330

    fix expert user notification

--- app/Http/Controllers/Admin/Complaint/MyComplaintController.php
+++ app/Http/Controllers/Admin/Complaint/MyComplaintController.php
@@ -141,7 +141,7 @@ class MyComplaintController extends Controller
         
         $department = $complaint->department->title;
 
-        $userPermission = Permission::where("key", "manage_complaint")->first()->users()->get();
+        $userPermission = Permission::where("key", "manage_complaint")->first()->users()->whereNotNull('mobile')->get();
 
         $userIds = $userPermission->pluck('id')->toArray();
 
