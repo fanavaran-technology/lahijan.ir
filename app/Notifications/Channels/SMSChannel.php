6ddb20ff971ec3b4b7bbba0534ee0817ce4f4031
yasin taghavi <mrtqvi@yahoo.com>
2023-11-18 12:20:13 +0330

    fix sms

--- app/Notifications/Channels/SMSChannel.php
+++ app/Notifications/Channels/SMSChannel.php
@@ -62,11 +62,5 @@ class SMSChannel
         $response = curl_exec($curl);
         $err = curl_error($curl);
         curl_close($curl);
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
     }
 }
